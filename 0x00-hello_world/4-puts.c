de6d19741e6f5af4c412e314697ea93574b54323
Opemipo Akintunde <opemipoakintunde29@gmail.com>
2023-02-16 10:13:32 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: a programme that prints a line using puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
