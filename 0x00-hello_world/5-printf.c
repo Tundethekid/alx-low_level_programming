3f32c559f0493e161686588dc681b8103cb03f0e
Opemipo Akintunde <opemipoakintunde29@gmail.com>
2023-02-16 10:21:54 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - entry point
+ * Description: a programme that prints a line using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
