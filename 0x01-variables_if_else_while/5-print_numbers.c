c0047987bfcb9c47d239cc528b142feb5fc7985c
Opemipo Akintunde <opemipoakintunde29@gmail.com>
2023-02-17 10:50:09 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits numbers of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int lm;
+
+	for (lm = 0; lm <= 9; lm++)
+		printf("%d", num);
+
+	printf("\n");
+	return (0);
+}
