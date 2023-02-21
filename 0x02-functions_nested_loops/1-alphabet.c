023264d7dfee1c7bbbd6de1c3447e9b02522e8da
Opemipo Akintunde <opemipoakintunde29@gmail.com>
2023-02-21 11:18:10 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+void print_alphabet(void);
+{
+	char la;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(la);
+
+	_putchar('\n');
+}
